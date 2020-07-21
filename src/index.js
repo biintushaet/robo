2903e5268d576f3a6a4cd30e22d5e2961c3b27d5
Biintushaet <biintushaet@gmail.com>
2020-07-21 14:43:20 +0600

    Updated for the first time

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
+import 'tachyons';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello greeting={'Ract Ninja'}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
