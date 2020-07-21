ceaff0b72d673153f46bda0a135cfc1908904784
Biintushaet <biintushaet@gmail.com>
2020-07-21 19:15:18 +0600

    added all the robots

--- src/robots.js
+++ src/robots.js
@@ -43,7 +43,7 @@ export const robots = [
   },
   {
     id: 8,
-    name: 'Nicholas Runolfsdottir V',
+    name: 'Nicholas Runol V',
     username: 'Maxime_Nienow',
     email: 'Sherwood@rosamond.me'
   },
