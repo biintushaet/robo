cc4a7e33efdcaa4bfdfcd9b364aeb1fb9a208440
Biintushaet <biintushaet@gmail.com>
2020-07-23 12:04:02 +0600

    second update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
-import {robots} from './robots';
+import App from './App';
 import 'tachyons';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-  <CardList robots={robots} />
-  </React.StrictMode>,
+  	<React.StrictMode>
+  		<App />
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
