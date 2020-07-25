a8adcbca41b979bc70dff748203801ab3b6e2179
Biintushaet <biintushaet@gmail.com>
2020-07-25 19:15:53 +0600

    Updates

---
+++ precache-manifest.ded0ded2bc52846ff87fae0e366c95bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f2f20572700dabd800e6ad2020588ef",
+    "url": "/robo/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "442bce476d1ab5e2ee5e",
+    "url": "/robo/static/css/main.c194ccfb.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robo/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "442bce476d1ab5e2ee5e",
+    "url": "/robo/static/js/main.19f73440.chunk.js"
+  },
+  {
+    "revision": "af839f1de40b3e92ec6c",
+    "url": "/robo/static/js/runtime-main.99a06c5d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
